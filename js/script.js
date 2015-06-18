eda4fb470a4c913aac5ba7e0d89825f140c19227
Chloe Eghtebas <nceghtebas@Chloes-MacBook-Pro.local>
2015-06-18 12:09:05 +0200

    canvas resizes with window

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
+var canvas = document.getElementById("canvas_1");
+
 function init() {
-	var canvas = document.getElementById("canvas_1");
-	canvas.width  = window.innerWidth;
-	console.log(canvas.width);
+  canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
-	canvas.addEventListener("mousedown", mouseDown, false);
+  canvas.addEventListener("mousedown", mouseDown, false);
+}
+
+function resizeCanvas() {
+  canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
+    console.log("w x h: "+ canvas.width + " " +canvas.height);
 }
 
 function mouseDown(event) {
-	canvas_x = event.pageX;
-	canvas_y = event.pageY;
-	console.log("x: "+ canvas_x + " y: " + canvas_y);
+  canvas_x = event.pageX;
+  canvas_y = event.pageY;
+  console.log("x: "+ canvas_x + " y: " + canvas_y);
 }
+
+
