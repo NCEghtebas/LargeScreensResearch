e8a2972213d410b6a693e9f268e186519d688ed6
Chloe Eghtebas <nceghtebas@hotmail.com>
2015-07-01 16:41:56 +0200

    using kinetic framework

--- js/script.js
+++ js/script.js
@@ -1,102 +1,47 @@
-var canvas = document.getElementById("canvas_1");
-var context = canvas.getContext('2d');
-var data = canvas.toDataURL();
-var img = new Image();
-var winIDList = [];
+var stage = new Kinetic.Stage({
+  container: 'container',
+  width: 1000,
+  height: 500
+});
 
-var menuX = window.innerWidth;
-var menuY = 7/8 * window.innerHeight;  
-var windowHeight = window.innerHeight;
 
-function init () {
-  canvas.width  = window.innerWidth;
-  canvas.height = window.innerHeight;
-  canvas.addEventListener("mousedown", mouseDown, false);
-  makeMenu();
+var reorderLayer = new Kinetic.Layer();
+var gridLayer = new Kinetic.Layer();
+
+var circle = new Kinetic.Circle({
+  x: 100,
+  y: stage.height()/2,
+  // sides: 6,
+  radius: 70,
+  fill: '#00D2FF',
+  stroke: 'black',
+  strokeWidth: 4,
+  draggable: true
+});
+
+function init() {
 }
 
-function resizeCanvas () {
-  canvas.width  = window.innerWidth;
-  canvas.height = window.innerHeight;
-  // console.log("w x h: "+ canvas.width + " " +canvas.height);
-  makeMenu();
-  context.restore();
-  
-  data = canvas.toDataURL();
-
-  console.log(data);
-
-  // scale and redraw the canvas content
-  img = new Image();
-  img.onload = function () {
-    context.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
-    }
-  img.src = data;
+function resizeCanvas() {
 }
 
-function makeMenu (x, y){
-  menuX = window.innerWidth;
-  menuY = 7/8 * window.innerHeight;  
-  windowHeight = window.innerHeight;
-  context.strokeRect(0, menuY, menuX, windowHeight);
-  context.stroke();
+// Calculates Circle Size based on window size
+function calculateCircleSize(){
+  //TODO: think of a justifiable way to do so 
 }
 
-function mouseDown (event) {
-  canvas_x = event.pageX;
-  canvas_y = event.pageY;
-  // console.log("x: "+ canvas_x + " y: " + canvas_y);
-  if (event.pageY >= menuY) {
-    openWindow();
-  }
-}
+// TODO add text inside circle
 
-// function contains(a, obj) {
-//     for (var i = 0; i < a.length; i++) {
-//         if (a[i] === obj) {
-//             return true;
-//         }
-//     }
-//     return false;
+// TODO configure circle placement or randomly layout the circles. 
+
+// How to make a function continuously check if circle is in correct order and change circle color from red to green 
+// gridLayer.add(grid);
+
+// function mouseDown(event) {
+//   canvas_x = event.pageX;
+//   canvas_y = event.pageY;
+//   console.log("x: "+ canvas_x + " y: " + canvas_y);
 // }
 
-// function createWinID(){
-// 	var winID = "window"+ Math.round((Math.random()*100;
-// 	if (!contains(winID, winIDList)){
-// 		winIDList.push("window"+ Math.round((Math.random()*100)));
-// 	}else{
-// 		createWinID();
-// 	}
-// }
-
-function openWindow(){
-  div = document.createElement("canvas_1");
-  div.style.width = "100px";
-  div.style.height = "100px";
-  div.style.background = "red";
-  div.style.color = "white";
-  div.innerHTML = "Hello";
-
-  document.body.appendChild(div);
-
-  var appX = Math.random()*menuX;
-  var appY = Math.random()*menuY;
-  var appWidth = Math.random()*window.innerWidth;
-  var appHeight = Math.random()*window.innerHeight;
-  // randomCoordinate(appX, appY);
-  // console.log(appX + " " + appY);
-  createRandomShape(appX, appY, appWidth, appHeight);
-}
-
-function createRandomShape(appX, appY, appWidth, appHeight){
-  context.strokeRect(appX, appY, appX+appWidth, appY+appHeight);
-  context.stroke();
-  context.save();
-  // console.log(context);
-}
-
-
-// function randomCoordinate(){
-//   appX = (Math.random()*menuX);
-//   appY = (Math.random()*menuY); 
-// }
+reorderLayer.add(circle);
+stage.add(reorderLayer).add(gridLayer);
