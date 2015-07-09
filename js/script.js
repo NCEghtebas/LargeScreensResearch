7e43078d72c3275805ad5aa4513af3b75e77f085
Chloe Eghtebas <nceghtebas@hotmail.com>
2015-07-09 11:35:37 +0200

    fucntion that calculates flexible grids centerpoitns

--- js/script.js
+++ js/script.js
@@ -45,39 +45,43 @@ function grid(){
     gDiv.id = 'grid';
 }
 
+// Untested
+function findCircleCenter(circle){
+  width = circle.style.width;
+  height = circle.style.height;
+  return width/2, height/2;
+}
+
+
 var circle_number = 48 ;
 var my_div = null;
 var mainDiv = null;
 var column = null;
 var sq = null;
-var position_array = calculateGrid();
-
-function calculateGrid(){
-    
-}
+// var position_array = calculateGrid();
 
 //TODO: make border only 1 px thick
-function generateGrid(r, c)
+function generateGrid(number_of_rows, number_of_columns)
 {
   container = document.getElementById('container');
   var i=0,id;
   var k= 1;
-  for(j=0; j<c; j++) {
+  for(j=0; j<number_of_columns; j++) {
       column = document.createElement("div");
       column.setAttribute('id', 'Column' + (j+1));  
       column.setAttribute('style', 'float:left; border:1px solid black');
-      column.style.width = String(Math.round(1/c*100)) + '%';//window.innerWidth/c;
+      column.style.width = String(Math.round(1/number_of_columns*100)) + '%';//window.innerWidth/c;
       column.style.height = '100%';
 
-      for(i=0; i<r; i++) {
-          sq = document.createElement("div");
-          sq.setAttribute('id', "sq" + k);  
-          sq.setAttribute('style', 'border:1px solid black');
-          sq.style.height = String(Math.round(1/r*100)) +'%';
-          sq.style.width = '100%';
+      for(i=0; i<number_of_rows; i++) {
+          square = document.createElement("div");
+          square.setAttribute('id', "Square" + k);  
+          square.setAttribute('style', 'border:1px solid black');
+          square.style.height = String(Math.round(1/number_of_rows*100)) +'%';
+          square.style.width = '100%';
 
           k++;
-          column.appendChild(sq);
+          column.appendChild(square);
       }
 
       container.appendChild(column);
@@ -92,6 +96,7 @@ function generateCircles(n){
     for(i=0; i<n; i++){
         circle = document.createElement("div");
         circle.setAttribute('id', 'Circle'+ (i+1));
+        //TODO: make radius of circle change with screen size
         circle.style.width = 100;
         circle.style.height = 100; 
         circle.style.borderRadius= 100;
@@ -102,6 +107,34 @@ function generateCircles(n){
     }
 }
 
+// Doesn't include 0
+function naturalNumberSequence(cardinality){
+  var a = [];
+  for (var i = 1; i <= cardinality; i++) {
+    a.push(i);
+  };
+  return a;
+}
+
+function getGridCenterPoints(number_of_rows, number_of_columns){
+  var centerPointArray = [];
+  var naturalNumbers = naturalNumberSequence(number_of_columns*number_of_rows);
+  var i = 0; 
+  for(c= 0; c< number_of_columns; c++){
+    for(r= 0; r< number_of_rows; r++){
+      var squareElement = document.getElementById('Square'+naturalNumbers[i]);
+      var rect = squareElement.getBoundingClientRect();
+      centerPointArray.push({x: rect.width/2 + rect.left, y: rect.height/2 + rect.top});
+      // console.log(coordinate.x, coordinate.y);
+      // console.log( 'Square'+naturalNumbers[i]+ ": "+ (rect.height/2 +rect.top));
+      // console.log(squareElement.getAttribute('id'));
+
+      i++;
+    }
+  }
+  return centerPointArray;
+}
+
 function greaterThan(val1, val2){
   if(val1 > val2){
     return val1;
@@ -110,12 +143,29 @@ function greaterThan(val1, val2){
   }
 }
 
-
-console.log()
-
 // generateCircles(circle_number);
 
 generateGrid(4,12);
+points = getGridCenterPoints(4, 12);
+
+console.log(points);
+
+/* CODE GRAVEYARD
+
+function oddNumSequence(length){
+  var odds = [];
+  for(i=0; i<((length-1)*2); i++){
+    if((i%2)){
+      odds.push(i);
+    }
+  } 
+  return odds;
+}
+
+// number_of_columns-1
+var odds = oddNumSequence(11);
+
+*/
 
 
 
