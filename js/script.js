818c2e3d028354fc668e5cea57727c4781546396
Chloe Eghtebas <nceghtebas@hotmail.com>
2015-07-22 12:13:49 +0200

    fixed offset bug and cleaned code

--- js/script.js
+++ js/script.js
@@ -55,17 +55,13 @@ var mydragg = function(){
 function checkColor(circle) { 
   var circle_label = circle.id.slice(6,7);
   var circle_number = circle.id.slice(7, circle.id.length);
-  // console.log("circle number: ",circle_number);
   var basket_label = circleInBasket(circle);
-  // console.log(basket_label);
   if (circle_label == basket_label) {
     // make circle color green
     changeCircleColorToGreen(circle_label, circle_number);
-    console.log("green");
   } else {
     // make circle color red
     changeCircleColorToRed(circle_label, circle_number);
-    console.log("red");
   }
 }
 
@@ -78,16 +74,11 @@ function circleInBasket(circle) {
   var rect_dummy = square_dummy.getBoundingClientRect();
   var i = 0;
   for (point in basket_points) {
-    // console.log(point);
     x1 = (basket_points[point].x);
     y1 = (basket_points[point].y);
     x2 = x1 + rect_dummy.width * 3;
     y2 = y1 + rect_dummy.height * 2;
-    // console.log( "x1: ", x1 , " <= xp: ", xp ,  " < x2: ", x2, " evaluates to: ", (x1 <= xp) && (xp < x2));
-    // console.log( "y1: ", y1 , " <= yp: ", yp ,  " < y2: ", y2, " evaluates to: ", (y1 <= yp) && (yp < y2));
-    // console.log( "end result: ", ( (x1 <= xp) && (xp < x2) ) && ( (y1 <= yp) && (yp < y2) ));
     if ( ( (x1 <= xp) && (xp < x2) ) && ( (y1 <= yp) && (yp < y2) ) ) {
-      // console.log(basket_initial_alphabet2[i]);
       return basket_initial_alphabet2[i];
     }
     i++;
@@ -135,17 +126,17 @@ function lessThan(val1, val2) {
 
 // from http://stackoverflow.com/questions/8877249/generate-random-integers-with-probabilities
 function getRandom (results) {
-    var num = Math.random(),
-        s = 0,
-        lastIndex = weights.length - 1;
+  var num = Math.random(),
+    s = 0,
+    lastIndex = weights.length - 1;
 
-    for (var i = 0; i < lastIndex; ++i) {
-        s += weights[i];
-        if (num < s) {
-            return results[i];
-        }
+  for (var i = 0; i < lastIndex; ++i) {
+    s += weights[i];
+    if (num < s) {
+        return results[i];
     }
-    return results[lastIndex];
+  }
+  return results[lastIndex];
 };
 
 // returns nearest grid point to x1 and y1
@@ -158,8 +149,8 @@ function getNearestGridPoint(x1, y1) {
   for(var i = 0 ; i < points.length; i ++) {
     var dist = Math.sqrt(Math.pow(points[i].x - current_x, 2) + Math.pow(points[i].y-current_y, 2));
     if(dist < minDist) {
-        minDist = dist;
-        ans = points[i];
+      minDist = dist;
+      ans = points[i];
     }
   }
   return ans;
@@ -173,24 +164,24 @@ function generateGrid(number_of_rows, number_of_columns)
   var i=0,id;
   var k= 1;
   for(j=0; j<number_of_columns; j++) {
-      column = document.createElement("div");
-      column.setAttribute('id', 'Column' + (j+1));  
-      column.setAttribute('style', 'float:left; border:1px solid black'); //border:1px solid black
-      column.style.width = String(Math.round(1/number_of_columns*100)) + '%';//window.innerWidth/c;
-      column.style.height = '100%';
-      column_div_array.push(column);
-      for(i=0; i<number_of_rows; i++) {
-          square = document.createElement("div");
-          square.setAttribute('id', "Square" + k);  
-          square.setAttribute('style', 'border:1px solid black; text-align:center');
-          square.style.height = String(Math.round(1/number_of_rows*100)) +'%';
-          square.style.width = '100%';
-          square_div_array.push(square);
-          k++;
-          column.appendChild(square);
-      }
+    column = document.createElement("div");
+    column.setAttribute('id', 'Column' + (j+1));  
+    column.setAttribute('style', 'float:left; border:1px solid black'); //border:1px solid black
+    column.style.width = String(Math.round(1/number_of_columns*100)) + '%';//window.innerWidth/c;
+    column.style.height = '100%';
+    column_div_array.push(column);
+    for(i=0; i<number_of_rows; i++) {
+      square = document.createElement("div");
+      square.setAttribute('id', "Square" + k);  
+      square.setAttribute('style', 'border:1px solid black; text-align:center');
+      square.style.height = String(Math.round(1/number_of_rows*100)) +'%';
+      square.style.width = '100%';
+      square_div_array.push(square);
+      k++;
+      column.appendChild(square);
+    }
 
-      container.appendChild(column);
+    container.appendChild(column);
   }
   // add the newly created element and it's content into the DOM
   my_div = document.getElementById("org_div1");
@@ -215,36 +206,36 @@ function getGridPoints(number_of_rows, number_of_columns){
 
 // Generates circles out of divs and returns array of circle ID names
 function generateNCircles(n){
-    container = document.getElementById('container');
-    var circleIDs = [] 
-    for (i=0; i<n; i++){
-        circle = document.createElement("div");
-        circleIDs.push('Circle'+ (i+1));
-        circle.setAttribute('id', circleIDs[i]);
-        circle.setAttribute('style', 'position: absolute; background-color: red; border-radius: 100px; border: 3px solid black');
-        // setting circle's initial position
-        circle.style.top = points[i].y+'px';
-        circle.style.left = points[i].x+'px';
-        circle.style.width = determineDiameter();
-        circle.style.height = determineDiameter();
-        // Making Circle Draggable
-        circle.setAttribute('onmousedown' ,'mydragg.startMoving(this,"container",event);');
-        circle.setAttribute('onmouseup', 'mydragg.stopMoving(this, "container");');
-        // Creating tables for centering text in circles
-        var table = document.createElement("table");
-        table.setAttribute('id', "Table"+(i+1));
-        table.setAttribute('style', 'width: 100%; height:100%; text-align: center;');
-        var table_row = document.createElement('tr'), table_column = document.createElement('td');
-        table_column.setAttribute("id", "TableColumn" + (i+1));
-        table.appendChild(table_row); table_row.appendChild(table_column);
-        // Creating Text
-        var text = document.createTextNode(randomizeCircleLetters(hard_sort_alphabet));
-        table_column.style.fontSize = determineTextSize();
-        table_column.appendChild(text);
-        circle.appendChild(table);
-        container.appendChild(circle);
-    }
-    return circleIDs;
+  container = document.getElementById('container');
+  var circleIDs = [] 
+  for (i=0; i<n; i++){
+    circle = document.createElement("div");
+    circleIDs.push('Circle'+ (i+1));
+    circle.setAttribute('id', circleIDs[i]);
+    circle.setAttribute('style', 'position: absolute; background-color: red; border-radius: 100px; border: 3px solid black');
+    // setting circle's initial position
+    circle.style.top = points[i].y+'px';
+    circle.style.left = points[i].x+'px';
+    circle.style.width = determineDiameter();
+    circle.style.height = determineDiameter();
+    // Making Circle Draggable
+    circle.setAttribute('onmousedown' ,'mydragg.startMoving(this,"container",event);');
+    circle.setAttribute('onmouseup', 'mydragg.stopMoving(this, "container");');
+    // Creating tables for centering text in circles
+    var table = document.createElement("table");
+    table.setAttribute('id', "Table"+(i+1));
+    table.setAttribute('style', 'width: 100%; height:100%; text-align: center;');
+    var table_row = document.createElement('tr'), table_column = document.createElement('td');
+    table_column.setAttribute("id", "TableColumn" + (i+1));
+    table.appendChild(table_row); table_row.appendChild(table_column);
+    // Creating Text
+    var text = document.createTextNode(randomizeCircleLetters(hard_sort_alphabet));
+    table_column.style.fontSize = determineTextSize();
+    table_column.appendChild(text);
+    circle.appendChild(table);
+    container.appendChild(circle);
+  }
+  return circleIDs;
 }
 
 // Returns points of baskets at column c  
@@ -254,7 +245,7 @@ function appendBasketPointsOfColumn(c){
   var squaresInColumn = document.getElementById('Column'+ (c+1)).childNodes;
   var array_index = 0;
   for (m = 0 ; m < squaresInColumn.length; m ++){
-    if (m%2) {
+    if ( !(m%2) ) {
       var rectsInColumn = squaresInColumn[m].getBoundingClientRect();
       temp_basket_array[array_index] = {x: rectsInColumn.left, y: rectsInColumn.top };
     }
@@ -268,10 +259,9 @@ function getBasketPoints(number_of_rows, number_of_columns){
   var basket_array= [];
   var i = 0; 
   var j = 0;
-  var even = [2, 4, 6, 8];
-  var merged =[];
-  for (c= 0; c< number_of_columns; c++){
-      if ( !(c%3) && !(r %2) ) {
+  var r = 0;
+  for (c = 0; c < number_of_columns; c++){
+      if ( !(c%3) ) {
         basket_array.push(appendBasketPointsOfColumn(c));
         j++;
         i+=8;
@@ -394,7 +384,7 @@ function generateInitialGameStateCircles(initial_alphabet_config) {
               break;
             }
           case 'K':
-            // if this n circle being placed is the 4rth or 5th circle
+            // if this k circle being placed is the 4rth or 5th circle
             if ( last_two.indexOf(circle_point) != -1 ) { 
               // make this circle a random letter and initialize it as red
               var rand_letter = getRandom(hard_sort_alphabet);
@@ -427,7 +417,7 @@ function generateInitialGameStateCircles(initial_alphabet_config) {
               break;
             }
           case 'R':
-            // if this n circle being placed is the 4rth or 5th circle
+            // if this r circle being placed is the 4rth or 5th circle
             if ( last_two.indexOf(circle_point) != -1 ) { 
               // make this circle a random letter and initialize it as red
               var rand_letter = getRandom(hard_sort_alphabet);
@@ -471,33 +461,32 @@ function generateInitialGameStateCircles(initial_alphabet_config) {
 // Generates a green draggable circle at point with id Circle+label
 // and centered text of value label
 function generateCirclesAtPoint(point, label){
-    container = document.getElementById('container');
-    circle = document.createElement("div");
-    // console.log("label: ", label);6
-    circle.setAttribute('id', "Circle" + label);
-    circle.setAttribute('style', 'position: absolute; background-color: green; border-radius: 100px; border: 3px solid black');
-    // setting circle's initial position
-    circle.style.top = point.y + 'px';
-    circle.style.left = point.x + 'px';
-    circle.style.width = determineDiameter();
-    circle.style.height = determineDiameter();
-    // Making Circle Draggable
-    circle.setAttribute('onmousedown' ,'mydragg.startMoving(this,"container",event);');
-    circle.setAttribute('onmouseup', 'mydragg.stopMoving(this, "container");');
-    // Creating tables for centering text in circles
-    var table = document.createElement("table");
-    table.setAttribute('id', "Table" + label);
-    table.setAttribute('style', 'width: 100%; height:100%; text-align: center;');
-    var table_row = document.createElement('tr'), table_column = document.createElement('td');
-    table_column.setAttribute("id", "TableColumn" +  label);
-    table.appendChild(table_row); table_row.appendChild(table_column);
-    // Creating Text
-    var text = document.createTextNode(label);
-    table_column.style.fontSize = determineTextSize();
-    // Adding everything
-    table_column.appendChild(text);
-    circle.appendChild(table);
-    container.appendChild(circle);
+  container = document.getElementById('container');
+  circle = document.createElement("div");
+  circle.setAttribute('id', "Circle" + label);
+  circle.setAttribute('style', 'position: absolute; background-color: green; border-radius: 100px; border: 3px solid black');
+  // setting circle's initial position
+  circle.style.top = point.y + 'px';
+  circle.style.left = point.x + 'px';
+  circle.style.width = determineDiameter();
+  circle.style.height = determineDiameter();
+  // Making Circle Draggable
+  circle.setAttribute('onmousedown' ,'mydragg.startMoving(this,"container",event);');
+  circle.setAttribute('onmouseup', 'mydragg.stopMoving(this, "container");');
+  // Creating tables for centering text in circles
+  var table = document.createElement("table");
+  table.setAttribute('id', "Table" + label);
+  table.setAttribute('style', 'width: 100%; height:100%; text-align: center;');
+  var table_row = document.createElement('tr'), table_column = document.createElement('td');
+  table_column.setAttribute("id", "TableColumn" +  label);
+  table.appendChild(table_row); table_row.appendChild(table_column);
+  // Creating Text
+  var text = document.createTextNode(label);
+  table_column.style.fontSize = determineTextSize();
+  // Adding everything
+  table_column.appendChild(text);
+  circle.appendChild(table);
+  container.appendChild(circle);
 }
 
 // Currently does nothing
@@ -540,6 +529,13 @@ function resizeCanvas() {
   // resizeBaskets();
 }
 
+// function colorCheckOnCreate() {
+//   // for (name of circleNames) {
+//   //   console.log(name);
+//   // }s
+//   console.log(circleNames)
+// }
+
 // one initialization alphabet determining basket labels
 var basket_initial_alphabet2 = [ 'H', 'R', 'H', 'N',
                                  'K', 'H', 'R', 'K',
@@ -579,7 +575,11 @@ var basket_points =  getBasketPoints(grid_rows, grid_columns);
 generateBaskets();
 
 // Finally we generate the circle game elements to be sorted
-var circleNames = generateInitialGameStateCircles(circle_number);
+generateInitialGameStateCircles(circle_number);
+
+// check color of each circle once created
+
+
 
 // TODO fix physics dragging/highlighting issue... 
 
